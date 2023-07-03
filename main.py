73ca1c749552e7d293a289e7139e05ffcd78650f
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-04 00:35:11 +0300

    sum/dot

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+
+    x = int(input("x: "))
+    y = int(input("y: "))
+    z = int(input("z: "))
+
+ 
+    print("Сума чисел:", x + y + z)
+    print("Добуток чисел:", x * y * z)
