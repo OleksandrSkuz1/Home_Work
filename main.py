061a860547abc4be55975fae7d77d72ccb9dc77d
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-04 18:10:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 if __name__ == '__main__':
 
-    x = int(input("x: "))
-    y = int(input("y: "))
-    z = int(input("z: "))
+     # # suma i dodatok chysel
+    #
+    # x = int(input("x: "))
+    # y = int(input("y: "))
+    # z = int(input("z: "))
+    #
+    # print("Сума чисел:", x + y + z)
+    # print("Добуток чисел:", x * y * z)
 
- 
-    print("Сума чисел:", x + y + z)
-    print("Добуток чисел:", x * y * z)
+
+    # # zalyshok zarplaty
+    #
+    # zarplata = int(input("Введіть зарплату за місяць : "))
+    # kredyt = int(input("Введіть кредит : "))
+    # komunalka = int(input("Введіть комунальні послуги : "))
+    # result = zarplata - kredyt - komunalka
+    #
+    # print("Сума,яка залишилась: ", result)
+
+    # # ploscha romba
+    #
+    # a = int(input('ВВедіть діагональ a = '))
+    # b = int(input('ВВедіть діагональ b = '))
+    #
+    # result = (a*b)/2
+    #
+    # print('Площа ромба: ', result)
+
+
+
